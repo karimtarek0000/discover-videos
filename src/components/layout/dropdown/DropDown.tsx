9c6e3777e43506755df461b9865b8b06add18963
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2023-08-02 22:11:20 +0300

    Login

--- src/components/layout/dropdown/DropDown.tsx
+++ src/components/layout/dropdown/DropDown.tsx
@@ -2,13 +2,19 @@ import Link from "next/link";
 import { useState } from "react";
 import Style from "./dropdown.module.css";
 import RenderSVG from "@/components/shared/RenderSVG";
+import { useRouter } from "next/router";
 
 const { dropdown, dropdownList } = Style;
 
 const DropDown = (): JSX.Element => {
+  const router = useRouter();
   const [toggle, setToggle] = useState<boolean>(false);
 
   const toggleHandler = () => setToggle(!toggle);
+
+  const logoutHandler = () => {
+    router.replace("/login");
+  };
 
   return (
     <div className={dropdown}>
@@ -20,7 +26,7 @@ const DropDown = (): JSX.Element => {
       {toggle && (
         <div className={dropdownList}>
           <RenderSVG name="logout" size="1rem" />
-          <Link href="/logout">Logout</Link>
+          <button onClick={logoutHandler}>Logout</button>
         </div>
       )}
     </div>
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,11 +1,49 @@
-import Image from "next/image";
-import Style from "../../styles/login.module.css";
 import Logo from "@/components/shared/logo/Logo";
 import Head from "next/head";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { ChangeEvent, ChangeEventHandler, useEffect, useState } from "react";
+import Style from "../../styles/login.module.css";
 
 const { formWrapper, title, inputWrapper, loginBtn } = Style;
 
 const Login = (): JSX.Element => {
+  const router = useRouter();
+  const [email, setEmail] = useState<string>();
+  const [statusEmail, setStatusEmail] = useState<boolean>(false);
+  const [err, setErr] = useState<string>("");
+
+  useEffect(() => {
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (email && regex.test(email)) {
+      setStatusEmail(true);
+      setErr("");
+    }
+
+    if (email && !regex.test(email)) {
+      setStatusEmail(false);
+      setErr("Email not a valid like | test@gmail.com");
+    }
+
+    if (email === "") {
+      setStatusEmail(false);
+      setErr("Email is required");
+    }
+  }, [email]);
+
+  const changeEmailHandler: ChangeEventHandler<HTMLInputElement> = (
+    e: ChangeEvent<HTMLInputElement>
+  ) => {
+    setEmail(e.target.value);
+  };
+
+  const loginHandler = (e: any) => {
+    e.preventDefault();
+
+    // router.replace("/");
+  };
+
   return (
     <main className="relative ">
       <Head>
@@ -38,14 +76,17 @@ const Login = (): JSX.Element => {
             <label htmlFor="email">Enter your email</label>
             <input
               autoComplete="off"
-              autoFocus
               id="email"
               type="email"
               placeholder="Enter your email"
+              onChange={changeEmailHandler}
             />
+            <p className="mt-2 text-center text-white">{err}</p>
           </div>
 
-          <button className={loginBtn}>Sign in</button>
+          <button onClick={loginHandler} disabled={!statusEmail} className={loginBtn}>
+            Sign in
+          </button>
         </form>
       </div>
     </main>
