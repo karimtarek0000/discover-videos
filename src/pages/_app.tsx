f9e631ffa1f45e8512a5ffa8e7799c1324535ed2
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2023-08-26 17:45:22 +0300

    middleware

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,36 +1,11 @@
-import useRouterEvents from "@/hooks/useRouterEvents";
-import { magic } from "@/lib/maginLinkClient";
 import "@/styles/globals.css";
 import { LazyMotion, domAnimation } from "framer-motion";
 import type { AppProps } from "next/app";
 import { Roboto } from "next/font/google";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 
 const inter = Roboto({ subsets: ["latin"], weight: ["400", "700"], variable: "--font-roboto" });
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  // const { loading } = useRouterEvents({ status: true });
-
-  // useEffect(() => {
-  //   (async () => {
-  //     try {
-  //       const isLoggedIn = await magic.user.isLoggedIn();
-  //       if (!isLoggedIn) {
-  //         router.replace("/login");
-  //       }
-  //       if (isLoggedIn && router.asPath === "/login") {
-  //         router.replace("/");
-  //       }
-  //     } catch (err) {
-  //       router.replace("/login");
-  //       console.log(err);
-  //     }
-  //   })();
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
-
   return (
     <>
       <style jsx global>{`
