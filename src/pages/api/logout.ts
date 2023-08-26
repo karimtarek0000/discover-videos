3b9f52b7c49dd85e041f8e2f2607a25893a5ecc3
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2023-08-26 22:19:06 +0300

    Logout api
    to remove cookie when user clicked on logout button

---
+++ src/pages/api/logout.ts
@@ -0,0 +1,17 @@
+import cookie from "cookie";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export async function logout(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "GET") {
+    const cookieOptions = {
+      expires: new Date(0),
+      httpOnly: true,
+    };
+
+    const setCookie = cookie.serialize("token", "", cookieOptions);
+
+    res.setHeader("Set-Cookie", setCookie);
+
+    res.status(200).json({ message: "logout sucessfully" });
+  }
+}
